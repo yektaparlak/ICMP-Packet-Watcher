324d4951e1977d2c25ad8701b5e29e5a9896fa4f
Yekta Parlak <yektaparlak@outlook.com>
2024-02-16 08:44:27 +0300

    Update icmp_Win32_API.cpp

--- Source Code Files (.cpp)/icmp_Win32_API.cpp
+++ Source Code Files (.cpp)/icmp_Win32_API.cpp
@@ -10,8 +10,8 @@
     2. IcmpSendEcho();    => Sends an IPv4 ICMP echo request and returns any echo response replies and the call returns when the time-out has expired or the reply buffer is filled.
     3. IcmpCloseHandle(); => Closes a handle opened by a call to the IcmpCreateFile or  IcmpCreateFile functions.*/
 
- /* To compile: g++ *.cpp -o pingapi.exe -lws2_32 -fPIC -static -static-libgcc -static-libstdc++ C:\Windows\System32\iphlpapi.dll
-    and then enter: ./pingapi DestinationIP (such as ./pingapi 1.1.1.1) */
+    /* To compile: g++ *.cpp -o pingapi.exe -lws2_32 -fPIC -static -static-libgcc -static-libstdc++ C:\Windows\System32\iphlpapi.dll
+       and then enter: ./pingapi DestinationIP (such as ./pingapi 1.1.1.1) */
 
 
 #include <winsock2.h>
@@ -33,36 +33,36 @@ bool CheckIPAddr(unsigned long ipaddr) {
 
 // Function to handle ICMP response status codes
 void HandleICMPStatus(int statusCode) {
-    switch(statusCode) {
-        case IP_SUCCESS:
-            cout << "Ping was successful!" << endl;
-            break;
-        case IP_DEST_NET_UNREACHABLE:
-            cout << "Destination network is unreachable." << endl;
-            break;
-        case IP_DEST_HOST_UNREACHABLE:
-            cout << "Destination host is unreachable." << endl;
-            break;
+    switch (statusCode) {
+    case IP_SUCCESS:
+        cout << "Ping was successful!" << endl;
+        break;
+    case IP_DEST_NET_UNREACHABLE:
+        cout << "Destination network is unreachable." << endl;
+        break;
+    case IP_DEST_HOST_UNREACHABLE:
+        cout << "Destination host is unreachable." << endl;
+        break;
         // Add more cases for other status codes
-        default:
-            cout << "Unknown ICMP status code: " << statusCode << endl;
-            break;
+    default:
+        cout << "Unknown ICMP status code: " << statusCode << endl;
+        break;
     }
 }
 
 // Function to handle errors from IcmpSendEcho
 void HandleICMPSendEchoError(DWORD errorCode) {
-    switch(errorCode) {
-        case IP_REQ_TIMED_OUT:
-            cout << "Ping request timed out." << endl;
-            break;
-        case IP_DEST_NET_UNREACHABLE:
-            cout << "Destination network is unreachable." << endl;
-            break;
+    switch (errorCode) {
+    case IP_REQ_TIMED_OUT:
+        cout << "Ping request timed out." << endl;
+        break;
+    case IP_DEST_NET_UNREACHABLE:
+        cout << "Destination network is unreachable." << endl;
+        break;
         // Add more cases for other error codes
-        default:
-            cout << "Unknown ICMP error code: " << errorCode << endl;
-            break;
+    default:
+        cout << "Unknown ICMP error code: " << errorCode << endl;
+        break;
     }
 }
 
@@ -149,6 +149,7 @@ int main(int argc, char** argv) {
             struct in_addr ipAddr {};
             ipAddr.S_un.S_addr = pEchoReply->Address;
 
+            cout << endl;
             cout << "Sent ICMP echo request to " << argv[1] << " with " << sizeof(SendData) << " Bytes payload" << endl;
 
             if (dwRetVal > 1) {
@@ -189,87 +190,87 @@ int main(int argc, char** argv) {
                         https://docs.microsoft.com/en-us/windows/win32/api/ipexport/ns-ipexport-icmp_echo_reply
                         https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes  */
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-// Routine Name:
-//
-//     IcmpCreateFile
-//
-// Routine Description:
-//
-//     Opens a handle on which ICMP Echo Requests can be issued.
-//
-// Arguments:
-//
-//     None.
-//
-// Return Value:
-//
-//     An open file handle or INVALID_HANDLE_VALUE. Extended error information
-//     is available by calling GetLastError().
-//
-////////////////////////////////////////////////////////////////////////////////////////////
-//
-// Routine Name:
-//
-//     IcmpCloseHandle
-//
-// Routine Description:
-//
-//     Closes a handle opened by ICMPOpenFile.
-//
-// Arguments:
-//
-//     IcmpHandle  - The handle to close.
-//
-// Return Value:
-//
-//     TRUE if the handle was closed successfully, otherwise FALSE. Extended
-//     error information is available by calling GetLastError().
-//
-////////////////////////////////////////////////////////////////////////////////////////////
-//
-// Routine Name:
-//
-//     IcmpSendEcho
-//
-// Routine Description:
-//
-//     Sends an ICMP Echo request and returns any replies. The
-//     call returns when the timeout has expired or the reply buffer
-//     is filled.
-//
-// Arguments:
-//
-//     IcmpHandle           - An open handle returned by ICMPCreateFile.
-//
-//     DestinationAddress   - The destination of the echo request.
-//
-//     RequestData          - A buffer containing the data to send in the
-//                            request.
-//
-//     RequestSize          - The number of bytes in the request data buffer.
-//
-//     RequestOptions       - Pointer to the IP header options for the request.
-//                            May be NULL.
-//
-//     ReplyBuffer          - A buffer to hold any replies to the request.
-//                            On return, the buffer will contain an array of
-//                            ICMP_ECHO_REPLY structures followed by the
-//                            options and data for the replies. The buffer
-//                            should be large enough to hold at least one
-//                            ICMP_ECHO_REPLY structure plus
-//                            MAX(RequestSize, 8) bytes of data since an ICMP
-//                            error message contains 8 bytes of data.
-//
-//     ReplySize            - The size in bytes of the reply buffer.
-//
-//     Timeout              - The time in milliseconds to wait for replies.
-//
-// Return Value:
-//
-//     Returns the number of ICMP_ECHO_REPLY structures stored in ReplyBuffer.
-//     The status of each reply is contained in the structure. If the return
-//     value is zero, extended error information is available via
-//     GetLastError().
-//
-////////////////////////////////////////////////////////////////////////////////////////////
+                        //////////////////////////////////////////////////////////////////////////////////////////////
+                        // Routine Name:
+                        //
+                        //     IcmpCreateFile
+                        //
+                        // Routine Description:
+                        //
+                        //     Opens a handle on which ICMP Echo Requests can be issued.
+                        //
+                        // Arguments:
+                        //
+                        //     None.
+                        //
+                        // Return Value:
+                        //
+                        //     An open file handle or INVALID_HANDLE_VALUE. Extended error information
+                        //     is available by calling GetLastError().
+                        //
+                        ////////////////////////////////////////////////////////////////////////////////////////////
+                        //
+                        // Routine Name:
+                        //
+                        //     IcmpCloseHandle
+                        //
+                        // Routine Description:
+                        //
+                        //     Closes a handle opened by ICMPOpenFile.
+                        //
+                        // Arguments:
+                        //
+                        //     IcmpHandle  - The handle to close.
+                        //
+                        // Return Value:
+                        //
+                        //     TRUE if the handle was closed successfully, otherwise FALSE. Extended
+                        //     error information is available by calling GetLastError().
+                        //
+                        ////////////////////////////////////////////////////////////////////////////////////////////
+                        //
+                        // Routine Name:
+                        //
+                        //     IcmpSendEcho
+                        //
+                        // Routine Description:
+                        //
+                        //     Sends an ICMP Echo request and returns any replies. The
+                        //     call returns when the timeout has expired or the reply buffer
+                        //     is filled.
+                        //
+                        // Arguments:
+                        //
+                        //     IcmpHandle           - An open handle returned by ICMPCreateFile.
+                        //
+                        //     DestinationAddress   - The destination of the echo request.
+                        //
+                        //     RequestData          - A buffer containing the data to send in the
+                        //                            request.
+                        //
+                        //     RequestSize          - The number of bytes in the request data buffer.
+                        //
+                        //     RequestOptions       - Pointer to the IP header options for the request.
+                        //                            May be NULL.
+                        //
+                        //     ReplyBuffer          - A buffer to hold any replies to the request.
+                        //                            On return, the buffer will contain an array of
+                        //                            ICMP_ECHO_REPLY structures followed by the
+                        //                            options and data for the replies. The buffer
+                        //                            should be large enough to hold at least one
+                        //                            ICMP_ECHO_REPLY structure plus
+                        //                            MAX(RequestSize, 8) bytes of data since an ICMP
+                        //                            error message contains 8 bytes of data.
+                        //
+                        //     ReplySize            - The size in bytes of the reply buffer.
+                        //
+                        //     Timeout              - The time in milliseconds to wait for replies.
+                        //
+                        // Return Value:
+                        //
+                        //     Returns the number of ICMP_ECHO_REPLY structures stored in ReplyBuffer.
+                        //     The status of each reply is contained in the structure. If the return
+                        //     value is zero, extended error information is available via
+                        //     GetLastError().
+                        //
+                        ////////////////////////////////////////////////////////////////////////////////////////////
